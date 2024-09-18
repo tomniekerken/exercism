887001a0b98963bc4da65a30daf5473663e38ec6
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2024-09-18 14:58:15 +0200

    matrix

---
+++ javascript/matrix/matrix.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Matrix' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Matrix {
+  constructor(input) {
+    this.matrix = input
+  }
+
+  get rows() {
+    console.log(this.matrix)
+  }
+
+  get columns() {
+    console.log(this.matrix)
+  }
+}
