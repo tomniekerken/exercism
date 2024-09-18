8944e0882644a81acb67ecdcb2be93a798bbc92a
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2024-09-18 23:14:21 +0200

    Solved collatz-conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
+export const steps = (number) => {
+  let steps = 0;
+
+  if (number <= 0) {
+    throw new Error("Only positive numbers are allowed");
+  }
+
+  if (number === 1) {
+    return steps;
+  }
+
+  for (let i = 0; number !== 1; i++) {
+    if (number % 2 === 0) {
+      number = number / 2;
+    } else {
+      number = 3 * number + 1;
+    }
+
+    steps += 1;
+  }
+
+  return steps;
 };
