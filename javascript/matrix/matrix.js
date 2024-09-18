219eebec9af8866971cf53cb5f6c27802c8704e8
tomniekerken <104563013+tomniekerken@users.noreply.github.com>
2024-09-18 18:01:49 +0200

    Solved matrix

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -3,16 +3,41 @@
 // convenience to get you started writing code faster.
 //
 
+// Input: matrix = "1"
+// Get Rows: rows[0] = 1
+
 export class Matrix {
-  constructor(input) {
-    this.matrix = input
+  constructor(matrix) {
+    this.matrix = matrix;
   }
 
   get rows() {
-    console.log(this.matrix)
+    let rows = this.matrix.split("\n");
+
+    const rowEntries = rows.map((row) => {
+      const numbersInRowAsString = row.split(" ").map(Number);
+
+      const numbersInRow = numbersInRowAsString.map((numberInRow) => {
+        return +numberInRow;
+      });
+
+      return numbersInRow;
+    });
+
+    return rowEntries;
   }
 
   get columns() {
-    console.log(this.matrix)
+    let columns = [];
+
+    for (let i = 0; i < this.rows[0].length; i++) {
+      columns.push([]);
+
+      this.rows.map((row) => {
+        columns[i].push(row[i]);
+      });
+    }
+
+    return columns;
   }
 }
